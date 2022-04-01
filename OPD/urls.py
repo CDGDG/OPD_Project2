b6a140d11f94b4844f1b1d4d39edf0c5fbae9418
mayoggu <lcj1050@naver.com>
2022-04-01 16:11:48 +0900

    company

--- OPD/urls.py
+++ OPD/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('developer/',include('developer.urls')),
     path('project/', include('project.urls')),
+    path('company/', include('company.urls')),
 ]
