442ed03b3df61302f10cb7020da383f3195ac313
CDGDG <whehdrjs207@naver.com>
2022-04-01 16:11:17 +0900

    OPD urls

--- OPD/urls.py
+++ OPD/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('developer/',include('developer.urls')),
+    path('admin/',include('developer.urls')),
     path('project/', include('project.urls')),
+    path('developer/',include('developer.urls')),
+    path('board/', include('project.urls')),
+    path('company/', include('project.urls')),
 ]
