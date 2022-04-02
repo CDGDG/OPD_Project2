fd94dab962fbdeaece69e2dbcacb6505e905c142
CDGDG <whehdrjs207@naver.com>
2022-04-03 00:09:59 +0900

    Project 기본 crud 완성

--- project/views.py
+++ project/views.py
@@ -1,12 +1,13 @@
 from functools import reduce
+from turtle import title
 from django.http import Http404
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.core.paginator import Paginator
 
 from admin.models import Language
 
 from .models import Project
-from .forms import Projectform
+from .forms import ProjectUpdateForm, Projectform
 
 def list(request):
     all_projects = Project.objects.all().order_by('-id')
@@ -33,13 +34,11 @@ def create(request):
             )
             project.save()  # many to many 넣어주기 전에 project의 pk가 필요하므로 미리 save
 
-            for pk in form.language:    # 선택한 언어 반복
-                if not pk: continue
-                _language = Language.objects.get(pk = pk) # 선택한 language의 pk로 language 정보 가져오기
+            for _language in form.language:    # 선택한 언어 반복
+                if not _language: continue
                 project.language.add(_language) # many to many 추가
 
-
-            return redirect('/project/list/')
+            return redirect(f'/project/detail/{project.pk}/')
         else:
             print(request.FILES)
 
@@ -54,12 +53,21 @@ def detail(request, pk):
     return render(request, 'project_detail.html', {'project': project})
 
 def update(request, pk):
-    try:
-        project = Project.objects.get(pk=pk)
-    except Project.DoesNotExist:
-        raise Http404('프로젝트를 찾을 수 없습니다.')
-    return render(request, 'project_update.html', {'project': project})
+    project = get_object_or_404(Project, pk=pk)
+    if request.method=="POST":
+        form = ProjectUpdateForm(request.POST, instance=project)
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.save()
+            return redirect(f"/project/detail/{pk}/")
+        else:
+            print('project:update - form 검증 False')
+    form = ProjectUpdateForm(instance=project)
+    return render(request, 'project_update.html', {'form': form, 'pk': pk})
 
 
 def delete(request):
-    pass
+    pk = request.POST.get('pk')
+    project = get_object_or_404(Project, pk=pk)
+    project.delete()
+    return render(request, 'project_delete_ok.html')
