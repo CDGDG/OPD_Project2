e99102267e3e1db39e36a69eccf014c903f51ca5
mayoggu <lcj1050@naver.com>
2022-04-09 00:03:39 +0900

    admin 로그인 로그아웃, notice CRUD 한바퀴~

--- admin/urls.py
+++ admin/urls.py
@@ -6,4 +6,9 @@ app_name = "Admin"
 urlpatterns = [
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
+    path('', views.adminlogin, name='adminlogin'),
+    path('notice/list/', views.noticelist, name='noticelist'),
+    path('notice/write/', views.noticewrite, name='noticewrite'),
+    path('notice/delete/', views.noticedelete, name='noticedelete'),
+    path('notice/detail/<int:pk>/', views.noticedetail, name='noticedetail'),
 ]
