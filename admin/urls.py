de2c034685e35de55336d7fbaebf9f839f98201a
hyewwon <rlagp424@gmail.com>
2022-04-09 00:48:21 +0900

    developer_list / admin_reset_password

--- admin/urls.py
+++ admin/urls.py
@@ -7,9 +7,10 @@ app_name = "Admin"
 urlpatterns = [
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
-    path('forgot_password/',views.check_userid, name="forgot_password"),
-    path('reset_password/', views.OPD_PasswordResetView.as_view(), name ='reset_password'),
-    path('reset_password_done/', views.OPD_PasswordResetDoneView.as_view(), name ='password_reset_done'),
+    path('checkuserid/',views.check_userid, name="check_userid"),
+    path('resetpassword/',views.reset_password, name="resetpassword"),
+    # path('reset_password/', views.OPD_PasswordResetView.as_view(), name ='reset_password'),
+    # path('reset_password_done/', views.OPD_PasswordResetDoneView.as_view(), name ='password_reset_done'),
     # path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name ='password_reset_confirm'),
     
 ]
--- admin/views.py
+++ admin/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render, redirect
-from django.urls import reverse_lazy
 from .forms import LoginForm
 from company.forms import Company
 from developer.forms import Developer
 from django.contrib.auth.hashers import make_password, check_password
 from django.http import Http404, JsonResponse
-from django.contrib.auth.views import PasswordResetView,PasswordResetDoneView
+
+# 비밀번호 찾기_이메일 전송
+from functools import reduce
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.core.mail import EmailMessage
+import string
+import random
 
 
 def home(request):
@@ -63,7 +69,7 @@ def logout(request):
             del(request.session['pic_url']) 
     return redirect('/') 
 
-
+# 이메일 전송을 위한 아이디 체크
 def check_userid(request):
     if request.method=="POST":
         context={}
@@ -85,14 +91,58 @@ def check_userid(request):
     else:
         return render(request,'forgot_password.html')
 
+# 이메일로 비밀번호 전송
+def reset_password(request):
+        who = request.POST.get('who')
+        userid = request.POST.get('userid')
 
-class OPD_PasswordResetView(PasswordResetView):
-    success_url = reverse_lazy('password_reset_done') # 성공시 호출 url
-    template_name = "forgot_password/password_reset_form.html" # 자신의 password_reset.html 이동
-    email_template_name = "forgot_password/password_reset_email.html"
-    def form_valid(self, form):
-        return super().form_valid(form)
+        # 비밀번호 생성
+        resetpassword = string.ascii_letters+string.digits
+        new_password = reduce(lambda p, x: p + random.choice(resetpassword), range(15), "")+"@"
 
-class OPD_PasswordResetDoneView(PasswordResetDoneView):
-    template_name = 'forgot_password/password_reset_done.html'
 
+        context={}
+        if who == "developer":
+            developer = Developer.objects.get(userid=userid)
+            mail_to = developer.email # 사용자 email
+            context['email'] = mail_to
+            developer.password = make_password(new_password) # 생성된 비밀번호 update
+            developer.save()
+        else:
+            company = Company.objects.get(companyid = userid)
+            mail_to = company.email
+            context['email'] = mail_to
+            company.password = make_password(new_password)
+            company.save()
+    
+        # 사용자에게 전송될 이메일 
+        current_site = get_current_site(request)
+        message = render_to_string('password_reset_email.html', { 
+            'domain': current_site.domain,
+            'new_password':new_password, # 이메일로 보내질 비밀번호
+            # 'developer': developer,
+            # 'uid': urlsafe_base64_encode(force_bytes(developer.pk)).encode().decode(),
+            # 'token': account_activation_token.make_token(developer),
+        })
+        mail_title = "Our Project Diary 비밀번호 변경" # 이메일 title
+        email = EmailMessage(mail_title, message, to=[mail_to])
+        try: 
+            email.send()
+        except:
+            context['fail'] = True # 이메일 전송 실패 시 
+
+        return render(request,'password_reset_done.html',context)
+
+
+
+# class OPD_PasswordResetView(PasswordResetView):
+#     success_url = reverse_lazy('password_reset_done') # 성공시 호출 url
+#     template_name = "forgot_password/password_reset_form.html" # 자신의 password_reset.html 이동
+#     # email_template_name = "forgot_password/password_reset_email.html"
+#     def form_valid(self, form):
+#         return super().form_valid(form)
+
+# class OPD_PasswordResetDoneView(PasswordResetDoneView):
+#     template_name = 'forgot_password/password_reset_done.html'
+
+    
