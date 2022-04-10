1bd53154127b32bdf76956cd0bb943bf52f1d162
CDGDG <whehdrjs207@naver.com>
2022-04-10 16:07:04 +0900

    dddeveloper

--- static/js/developer/developer_join.js
+++ static/js/developer/developer_join.js
@@ -68,106 +68,154 @@ $(document).ready(function(){
         let target = "";
         
         // 아이디
-        target='userid'
+        target="#id_userid"
         if(joinfrm['userid'].value.trim()==""){
             // joinfrm['userid'].focus();
+            $(target).siblings('label').addClass('wrongLabel')
             $('#check_userid').html('<p style="color:red">아이디를 입력해주세요</p>');
             // return false;
             first = first?first:target
         }
-        
-        if(!reg_id.test(joinfrm['userid'].value.trim())){
+        else if(!reg_id.test(joinfrm['userid'].value.trim())){
             // joinfrm['userid'].focus();
+            $(target).siblings('label').addClass('wrongLabel')
             $('#check_userid').html('<p style="color:red">유효한 아이디가 아닙니다</p>');
             first = first?first:target
             // return false;
         }
-        if(!id_button){
+        else if(!id_button){
             // joinfrm['userid'].focus();
+            $(target).siblings('label').addClass('wrongLabel')
             $('#check_userid').html('<p style="color:red">아이디 중복검사를 해주세요</p>');
             first = first?first:target
             // return false;
         }
+        else{
+            $(target).removeClass('is-invalid').addClass('is-valid').siblings('label').removeClass('wrongLabel')
+            $('#check_userid').html('');
+        }
+
         // 비밀번호
+        target="#id_password"
         if(joinfrm['password'].value.trim()==""){
-            joinfrm['password'].focus();
+            // joinfrm['password'].focus();
+            $(target).addClass('is-invalid').siblings('label').addClass('wrongLabel')
+            first = first?first:target
             $('#check_password').html('<p style="color:red">비밀번호를 입력해주세요</p>');
-            return false;
-            
+            // return false;
         }
-        
-        if(!reg_pass.test(joinfrm['password'].value.trim())){
-            joinfrm['password'].focus();
+        else if(!reg_pass.test(joinfrm['password'].value.trim())){
+            // joinfrm['password'].focus();
+            $(target).addClass('is-invalid').siblings('label').addClass('wrongLabel')
+            first = first?first:target
             $('#check_password').html('<p style="color:red">유효한 비밀번호가 아닙니다</p>');
-            return false;
+            // return false;
         }
-        
-        if(joinfrm['password'].value.trim() != joinfrm['re_password'].value.trim()){
-            joinfrm['re_password'].focus();
+        else{
+            $(target).removeClass('is-invalid').addClass('is-valid').siblings('label').removeClass('wrongLabel')
+        }
+
+        target = "#id_re_password"
+        if(joinfrm['password'].value.trim()=="" || joinfrm['password'].value.trim() != joinfrm['re_password'].value.trim()){
+            // joinfrm['re_password'].focus();
+            $(target).addClass('is-invalid').siblings('label').addClass('wrongLabel')
+            first = first?first:target
             $('#check_re_password').html('<p style="color:red">비밀번호가 다릅니다</p>');
-            return false;
+            // return false;
+        }
+        else{
+            $(target).removeClass('is-invalid').addClass('is-valid').siblings('label').removeClass('wrongLabel')
         }
         
+        // 닉네임
+        target = "#id_nickname"
         if(joinfrm['nickname'].value.trim()==""){
-            joinfrm['nickname'].focus();
+            // joinfrm['nickname'].focus();
+            $(target).siblings('label').addClass('wrongLabel')
+            first = first?first:target
             $('#check_nickname').html('<p style="color:red">닉네임을 입력해주세요</p>');
-            return false;
+            // return false;
         }
-        
-        if(joinfrm['nickname'].value.trim().length < 3){
-            joinfrm['nickname'].focus();
+        else if(joinfrm['nickname'].value.trim().length < 3){
+            // joinfrm['nickname'].focus();
+            $(target).siblings('label').addClass('wrongLabel')
+            first = first?first:target
             $('#check_nickname').html('<p style="color:red">닉네임을 세자리 이상입력해주세요</p>');
-            return false;
+            // return false;
 
         }
-
-        if(!nick_button){
-            joinfrm['nickname'].focus();
+        else if(!nick_button){
+            // joinfrm['nickname'].focus();
+            $(target).siblings('label').addClass('wrongLabel')
+            first = first?first:target
             $('#check_nickname').html('<p style="color:red">닉네임 중복검사를 해주세요</p>');
-            return false;
-            
+            // return false;
         }
+        else{
+            $(target).removeClass('is-invalid').addClass('is-valid').siblings('label').removeClass('wrongLabel')
+        }
+
+        //주민등록번호
+        target = "#id_registnum"
         if($('#id_registnum1').val() == "" || $('#id_registnum2').val() == ""){
-            $('#id_registnum1').focus();
+            // $('#id_registnum1').focus();
+            $(target).siblings('label').addClass('wrongLabel')
+            first = first?first:target
             $('#check_registnum').html('<p style="color:red">주민등록번호를 모두 입력해주세요</p>');
-            return false;
+            // return false;
         }else{
+            $(target).removeClass('is-invalid').addClass('is-valid').siblings('label').removeClass('wrongLabel')
             joinfrm['registnum'].value= $('#id_registnum1').val()+$('#id_registnum2').val();
         }
 
-
+        // 전화번호
+        target = "#id_phonenum"
         if($('#id_phonenum1').val() == "" || $('#id_phonenum2').val() == "" || $('#id_phonenum3').val() == ""){
-            $('#id_phonenum1').focus();
+            // $('#id_phonenum1').focus();
+            $(target).siblings('label').addClass('wrongLabel')
+            first = first?first:target
             $('#check_phonenum').html('<p style="color:red">휴대폰번호를 모두 입력해주세요</p>');
-            return false;
-            
+            // return false;
         }else{
+            $(target).removeClass('is-invalid').addClass('is-valid').siblings('label').removeClass('wrongLabel')
             joinfrm['phonenum'].value = $('#id_phonenum1').val()+"-"+$('#id_phonenum2').val()+"-"+$('#id_phonenum3').val();
         }
         
+        // 이메일
+        target = "#id_email"
         if($('#email_id').val() == "" || $('#email_option').val() == ""){
-            $('#email_id').focus();
+            // $('#email_id').focus();
             $('#check_email_id').html('<p style="color:red">이메일을 모두 입력해주세요</p>');
-            return false;
-
+            // return false;
         }
 
         if(!send_email){
-            joinfrm['send_email'].focus();
+            // joinfrm['send_email'].focus();
             $('#check_email_option').html('<p style="color:red">이메일 인증을 해주세요</p>');
-            return false;
+            // return false;
         }
         
+        // 언어
+        target = "#id_language"
         if(check_cnt<1){
-            $('#check_language').focus();
+            // $('#check_language').focus();
+            $(target).siblings('label').addClass('wrongLabel')
+            first = first?first:target
             $('#check_language').html('<p style="color:red">한개 이상의 언어를 선택해주세요 ')
-            return false;
+            // return false;
+        }
+        else{
+            $(target).removeClass('is-invalid').addClass('is-valid').siblings('label').removeClass('wrongLabel')
         }
        
-        console.log(check_cnt)
+        // console.log(check_cnt)
 
-        console.log(joinfrm['phonenum'].value);
-        console.log(joinfrm['registnum'].value);
-        // joinfrm.submit();
+        // console.log(joinfrm['phonenum'].value);
+        // console.log(joinfrm['registnum'].value);
+        if(first){
+            $(first).focus()
+        }else{
+            joinfrm.submit();
+        }
     })
 })
