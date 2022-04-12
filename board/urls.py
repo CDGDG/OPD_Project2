d1d0c8ab892113e23d8504d189d524e517e1a6a5
leejeongsoo1660 <74721370+leejeongsoo1660@users.noreply.github.com>
2022-04-12 17:37:35 +0900

    da

--- board/urls.py
+++ board/urls.py
@@ -10,5 +10,8 @@ urlpatterns = [
     path("detail/<int:pk>/", views.board_detail, name="detail"),
     path("update/<int:pk>/", views.board_update, name="update"),
     path("delete/", views.board_delete, name="delete"),
+    path("filedownload/<int:pk>/", views.filedownload, name='filedownload'),
+    path('commentwrite/<int:pk>/', views.comment_write, name='commentwrite'),
+    path('commentdelete/', views.comment_delete, name='commentdelete'),
     # path("c")
 ]
