5efa469c2f82b9be98403febc6b65e9cea91a775
hyewwon <rlagp424@gmail.com>
2022-04-13 16:17:36 +0900

    ㅣㅣㅣ

--- admin/views.py
+++ admin/views.py
@@ -202,8 +202,10 @@ def noticewrite(request):
                     img_original = request.FILES.get('file').name,
                 )
                 noticeimg.save()
+            else:
+                noticeimg = ""
 
-        return render(request, 'notice_detail.html', {'notice': notice})
+        return render(request, 'notice_detail.html', {'notice': notice,'noticeimg':noticeimg})
 
 
 def noticedelete(request):
